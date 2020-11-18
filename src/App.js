67af599737f8829f747ea2d2753f823887741b6c
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-18 18:29:00 +0600

    made a box inside the section hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/header';
+import Hero from './components/hero';
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 };
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./_styles.scss";
+import mouse from  "../../assets/mouse.svg";
+
 
 const  Hero = () => {
     return (
-        <section className="hero">
+        <section className="hero flex">
             <div className="container">
-                <div className="hero__box">
+                <div className="hero__box flex">
                     <h1 className="box__title">
                     Грузовые перевозки
                     </h1>
@@ -12,6 +15,7 @@ const  Hero = () => {
                     Перевозка крупногабаритных грузов по России и Европе. Полное экспедирование, страхование, упаковка и хранение грузов
                     </p>
                 </div>
+                <img src={mouse} alt="" className="hero__mouse"/>
             </div>
         </section>
     )
