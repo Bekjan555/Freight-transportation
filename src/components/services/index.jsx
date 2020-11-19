4b5a4be9ceddfb7e74c021c2327f2751ca63519a
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-20 01:18:02 +0600

    finished section "Services"

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -1,30 +1,67 @@
-import React from 'react';
+import React from "react";
 import "./_styles.scss";
 import aircraft from "../../assets/aircraft.svg";
 
-
 const Services = () => {
-    return (
-        <section className="services">
-            <div className="container">
-                <h2 className="services__title title">
-                    услуги
-                </h2>
-                <div className="services__content">
-                    <div className="services__row row">
-                        <div className="col-3"> <img src={aircraft} alt=""/><h3 className="aircraft__title"></h3></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                        <div className="col-3"> <img src={aircraft} alt=""/></div>
-                    </div>
-                </div>
+  return (
+    <section className="services">
+      <div className="container">
+        <h2 className="title">услуги</h2>
+        <div className="services__content">
+          <div className="services__row row">
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
             </div>
-        </section>
-    )
-}
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+            <div className="services__item flex col-3">
+              <div className="services__img">
+                <img className="services__icon" src={aircraft} alt="" />
+              </div>
+              <h3 className="aircraft__title">Авиаперевозки</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Services;
