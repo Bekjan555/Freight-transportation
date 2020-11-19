41030ad431813af517b48a84d551c6bf5345c27c
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-20 02:38:24 +0600

    inside the section "about" created columns with information

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./_styles.scss";
+import car2 from "../../assets/car-2.webp";
 
 const About = () => {
   return (
     <section className="about">
       <div className="container">
         <div className="about__content">
-          <div className="row">
+          <div className="about__row row">
             <div className="col-6">
               <h2 className="title">О компании</h2>
               <p className="about__description">
@@ -19,12 +20,28 @@ const About = () => {
                 веков, но и перешагнул в электронный дизайн.
               </p>
             </div>
-            <div className="col-6">
-                <div className="about__item col-3"></div>
-                <div className="about__item col-3"></div>
-                <div className="about__item col-3"></div>
-                <div className="about__item col-3"></div>
-                <div className="about__item col-3"></div>
+            <div className="col-6 row">
+              <div className="about__item col-3">
+                <h3 className="text-big">48</h3>
+                <p className="text-medium">офисов</p>
+                <span className="text-small">В Европе и СНГ</span>
+              </div>
+              <div className="about__item col-3">
+                <h3 className="text-big">361</h3>
+                <p className="text-medium">грузовиков</p>
+                <span className="text-small">Volvo, Scania</span>
+              </div>
+              <div className="about__item col-3">
+                <img src={car2} alt="" />
+              </div>
+              <div className="about__item col-3">
+                <img src={car2} alt="" />
+              </div>
+              <div className="about__item col-3">
+                <h3 className="text-big">1500</h3>
+                <p className="text-medium">сотрудников</p>
+                <span className="text-small">профессионалов</span>
+              </div>
             </div>
           </div>
         </div>
