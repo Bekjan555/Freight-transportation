deef04bafc8addc8597e7b2a5868d3406563722f
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-20 01:48:49 +0600

    inside the "about" section created and styled the title and description

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./_styles.scss";
 
 const About = () => {
-    return (
-        <section className="about">
-            <div className="container">
-                
+  return (
+    <section className="about">
+      <div className="container">
+        <div className="about__content">
+          <div className="row">
+            <div className="col-6">
+              <h2 className="title">О компании</h2>
+              <p className="about__description">
+                Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+                вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для
+                текстов на латинице с начала XVI века. В то время некий
+                безымянный печатник создал большую коллекцию размеров и форм
+                шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem
+                Ipsum не только успешно пережил без заметных изменений пять
+                веков, но и перешагнул в электронный дизайн.
+              </p>
             </div>
-        </section>
-    )
-}
+            <div className="col-6">
+                <div className="about__item col-3"></div>
+                <div className="about__item col-3"></div>
+                <div className="about__item col-3"></div>
+                <div className="about__item col-3"></div>
+                <div className="about__item col-3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About 
+export default About;
