e03f84fe3b217e0e7cf964bae1f1dedac924d675
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-21 00:24:23 +0600

    finished "footer"

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -9,12 +9,16 @@ const Footer = () => {
       <div className="container">
         <div className="footer__content flex">
           <img src={logo} alt="" />
+            <div className="footer__info">
+                <p>123456, Санкт-Петербург, Невский пр-кт 127</p>
+            <Link className="footer__link" href="#">sales@log.ru</Link>
+            </div>
+          <div className="footer__connection flex">
+                <span>+7 (812) 344-56-65</span>
+                <span>+7 (812) 344-56-65</span>
+          </div>
+          <Link className="footer__link ">Разработано - D-E-N.ru</Link>
         </div>
-        <p>123456, Санкт-Петербург, Невский пр-кт 127</p>
-        <Link href="#">sales@log.ru</Link>
-        <span>+7 (812) 344-56-65</span>
-        <span>+7 (812) 344-56-65</span>
-        <Link>Разработано - D-E-N.ru</Link>
       </div>
     </footer>
   );
